0154af4b0b5c39eb66a6fd792d2cfdd2c6ec18a5
Atomie CHEN <atomic_cwh@163.com>
2024-06-12 00:55:18 +0800

    feat(start.sh): minor improvements for output message

--- start.sh
+++ start.sh
@@ -8,13 +8,14 @@ cd $CURRENT_DIR
 
 
 # CHANGE THIS: the actual command you want to run
-CMD=
+CMD=""
 
 # check if CMD is set
 LIGHT_RED='\033[1;31m'
+LIGHT_CYAN='\033[1;36m'
 NC='\033[0m'
 if [ -z "$CMD" ]; then
-    printf "${LIGHT_RED}Nothing to run. Please set the CMD variable in start.sh${NC}\n"
+    printf "${LIGHT_RED}Nothing to run. Please set the CMD variable in ${LIGHT_CYAN}start.sh${NC}\n"
     exit 1
 fi
 
